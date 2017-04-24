c45a1b7fc41b5d3527b83d4bd7fba89681451e55
Ben Greenman <benjaminlgreenman@gmail.com>
2017-04-24 01:23:34 -0400

    [raco] simple 'new example' function
    Simple function to create a directory and files for a new example

--- main.rkt
+++ main.rkt
@@ -4,13 +4,63 @@
 ;; - defines the `syntax-parse-examples` raco command
 ;; - ???
 
+;; For now the philosophy is "keep things straight-line, avoid making functions
+;;  and new files"
+
+(require
+  racket/runtime-path)
+
+(define-runtime-path SPE-HOME ".")
+
+(define SPE 'syntax-parse-examples)
+
 ;; =============================================================================
 
-(define (make-new-example macro-name)
-  ;; make folder
-  ;; make 3 files (src, scrbl, -test
-  ;; edit index.scrbl
-  (displayln "make new example"))
+(define (make-new-example! macro-name)
+  (define macro-dir (make-example-directory! macro-name))
+  (make-example-src! macro-dir macro-name)
+  (make-example-doc! macro-dir macro-name)
+  (make-example-test! macro-dir macro-name)
+  (void))
+
+;; make-example-directory! : string? -> path-string?
+;; Create a directory to hold a new example.
+;; Raise exception if a directory with the given name already exists.
+(define (make-example-directory! macro-name)
+  (define macro-dir (build-path SPE-HOME macro-name))
+  (if (directory-exists? macro-dir)
+    (raise-user-error SPE "example '~a' already exists" macro-name)
+    (begin (make-directory macro-dir)
+           macro-dir)))
+
+(define (displayln* out-file . str*)
+  (with-output-to-file out-file #:exists 'replace
+    (λ () (for-each displayln str*))))
+
+(define (make-example-src! dir name)
+  (displayln* (path-replace-extension (build-path dir name) #".rkt")
+    "#lang racket/base"
+    "(require (for-syntax racket/base syntax/parse))"
+    ""
+    "#;(TODO fill in here)"))
+
+(define (make-example-doc! dir name)
+  (displayln* (path-replace-extension (build-path dir name) #".scrbl")
+    "#lang scribble/manual"
+    (format "@require[(for-label racket/base syntax/parse syntax-parse-examples/~a/~a.rkt" name name)
+    ""
+    (format "@title{~a}" name)
+    ""
+    "" #;(TODO put the code here!)
+    ))
+
+(define (make-example-test! dir name)
+  (displayln* (path-replace-extension (build-path dir name) #".test")
+    "#lang racket/base"
+    "(module+ test"
+    (format "  (require rackunit syntax-parse-examples/~a/~a.rkt)" name name)
+    ""
+    "#;(TODO add tests here)"))
 
 ;; =============================================================================
 
@@ -20,11 +70,13 @@
   (define-syntax-rule (do-something e* ...)
     (begin (*did-something?* #t) e* ...))
   (define (cmdline argv)
-    (parse-command-line "syntax-parse-examples" argv
+    ;; Using `parse-command-line` instead of the prettier `command-line`
+    ;;  so this function can call itself recursively
+    (parse-command-line (symbol->string SPE) argv
      `((once-any
         [("-n" "--new")
          ,(λ (flag macro-name)
-            (do-something (make-new-example macro-name)))
+            (do-something (make-new-example! macro-name)))
          ("Make a new example" "string?")
          ]))
      (λ ARG*
